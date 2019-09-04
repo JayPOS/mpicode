5989239a7dcb0795582f3ca6aeae51183ddb3ced
Gabriel P. Silva <gabriel.silva@mail.com>
2019-09-04 16:42:01 -0300

    Atualização

--- mpi_probe.c
+++ mpi_probe.c
@@ -14,24 +14,23 @@ MPI_Status estado;
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     /* Envia um número aleatório de dados */
     if (meu_ranque == origem) {
-        srand(MPI_Wtime()*meu_ranque);
+        srand(MPI_Wtime());
         total_num = (rand() / (float)RAND_MAX) * MAX;
         MPI_Send(numeros, total_num, MPI_INT, destino, etiq, MPI_COMM_WORLD);
-        printf("Processo %d enviou %d números  para processo %d.\n", origem, total_num, destino);
+        printf("Processo %d enviou %d números para o processo %d.\n", origem, total_num, destino);
     } 
     else 
         if (meu_ranque == destino) {
-    /* Testa para uma mensagem chegando do processo origem */
-            MPI_Probe(origem, etiq, MPI_COMM_WORLD, &estado);
+    /* Verifica se uma mensagem chegou do processo origem */
+	    MPI_Probe(origem, etiq, MPI_COMM_WORLD, &estado);
     /* Quando a rotina MPI_Probe retorna, o manipulador "estado" contém
        o tamanho da mensagem que chega. Obtém o tamanho da mensagem */
             MPI_Get_count(&estado, MPI_INT, &total_num);
     /* Aloca um vetor para guardar os números transmitidos */
             vet_num = (int*) malloc(sizeof(int)*total_num);
     /* Agora recebe a mensagem com o vetor previamente alocado  */
-            MPI_Recv(vet_num, total_num, MPI_INT, origem, etiq,
-             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Processo %d recebeu dinamicamente %d números de %d.\n", destino, total_num, origem);
+            MPI_Recv(vet_num, total_num, MPI_INT, origem, etiq,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Processo %d recebeu dinamicamente %d números de %d.\n", destino, total_num, origem);
             free(vet_num);
         }
     MPI_Finalize();
